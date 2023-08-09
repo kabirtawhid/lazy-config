bf407a04958ef9ffaef81ade74b19d259d65ce95
Kabir Tawhid <kabirfromtawhid@gmail.com>
2023-08-09 16:43:26 +0600

    Almost full fledged ide tailored to my need

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,15 @@
 
 --exit insert mode with jk
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true, desc = "<Esc>" })
+
+----  navigating code faster
+-- TODO: I need to map lsp keybingdin k to <leader>k for this to work
+vim.keymap.set({ "n", "v" }, "jd", "<c-b>", { noremap = false, desc = "Up faster" })
+vim.keymap.set({ "n", "v" }, "js", "<c-f>", { noremap = true, desc = "Down faster" })
+--
+----  Remap K and J
+--vim.keymap.set({ "n", "v" }, "<leader>k", "K", { desc = "Keyword" })
+--vim.keymap.set({ "n", "v" }, "<leader>j", "J", { noremap = true, desc = "Keyword" })
+
+-- Save using <leader> , command
+vim.keymap.set("n", "<leader>n", "<cmd>write<cr>", { noremap = true, desc = "write easily" })
--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,19 +1,28 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      tailwindcss = {
-        filetypes_exclude = { "markdown" },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {
+          filetypes_exclude = { "markdown" },
+        },
+      },
+      setup = {
+        tailwindcss = function(_, opts)
+          local tw = require("lspconfig.server_configurations.tailwindcss")
+          --- @param ft string
+          opts.filetypes = vim.tbl_filter(function(ft)
+            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+          end, tw.default_config.filetypes)
+        end,
       },
     },
-    setup = {
-      tailwindcss = function(_, opts)
-        local tw = require("lspconfig.server_configurations.tailwindcss")
-        --- @param ft string
-        opts.filetypes = vim.tbl_filter(function(ft)
-          return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
-        end, tw.default_config.filetypes)
-      end,
-    },
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "prettierd")
+    end,
   },
 }
